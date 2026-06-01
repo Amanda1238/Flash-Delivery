d506e8d27fa216ef6240e155ba962354379f63a2
AMANDA FERNANDA LOPES MARTINS <17amandamartins@gmail.com>
2026-06-01 16:13:02 -0300

    tirando o print
    Signed-off-by: AMANDA FERNANDA LOPES MARTINS <17amandamartins@gmail.com>

--- main.py
+++ main.py
@@ -34,8 +34,6 @@ print("Você será o responsável por comandar toda a operação.".center(largur
 print("=" * largura)
 print()
 print("Escolha uma das opções do menu principal para começar o jogo!")
-print()
-
 # variaveis auxiliares
 op = 20
 historico: List[Pedido] = carregar_historico(arquivo="savegame.json")
